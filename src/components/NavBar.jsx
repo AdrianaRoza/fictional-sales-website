ef0ec907812690dfc4a29608686b360ad0ee8310
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-07-21 10:40:53 -0400

    refactor: navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,15 +9,30 @@ const NavBar = () => {
   } 
 
   return(
-    <nav className="bg-gray-200 py-2 flex items-center justify-between">
-      <div className="h-10 px-2 flex items-center">
-        <img src="/tabua-de-cortar.png" alt="tabua-de-cortar" className='h-full' />
-        <h1 className='flex px-2  text-violet-500'>NST ART CNC</h1>
+    <nav 
+      className="bg-gray-200 py-2 flex 
+      items-center justify-between">
+
+      <div 
+        className="h-10 px-2 flex items-center">
+
+        <img 
+          src="/tabua-de-cortar.png" 
+          alt="tabua-de-cortar" 
+          className='h-full' 
+        />
+        <h1 
+          className='flex px-2  text-violet-500'>
+          NST ART CNC
+        </h1>
       </div>
 
-      <div className="lg:hidden">
-        <FaBars onClick={handleToggleNav} 
-        className="text-xl mr-3 cursor-pointer text-violet-500" />
+      <div 
+        className="lg:hidden">
+        <FaBars 
+          onClick={handleToggleNav} 
+          className="text-xl mr-3 cursor-pointer
+          text-violet-500" />
       </div>
 
         <ul 
@@ -35,13 +50,24 @@ const NavBar = () => {
           </li>
           
           <li>
-            <a href="#about" className="text-violet-500  hover:text-violet-700">
+            <a href="#about" 
+              className="text-violet-500  hover:text-violet-700">
               About
             </a>
           </li>
 
-          <li><a href="#services" className="text-violet-500  hover:text-violet-700	">Services</a></li>
-          <li><a href="#contact" className="text-violet-500  hover:text-violet-700	">contact</a></li>
+          <li>
+            <a href="#services" 
+              className="text-violet-500  hover:text-violet-700	">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#contact" 
+            className="text-violet-500  hover:text-violet-700	">
+              contact
+            </a>
+          </li>
         </ul>
     </nav>
   )
