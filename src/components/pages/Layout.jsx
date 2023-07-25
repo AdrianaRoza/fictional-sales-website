6fc79c96f9129fdf274ac30b2e15839936aa7773
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-07-25 15:07:03 -0400

    feat: create products layout

--- src/components/pages/Layout.jsx
+++ src/components/pages/Layout.jsx
@@ -3,11 +3,10 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return(
-    <>
+    <div className="flex flex-col min-h-screen">
       <NavBar />
-
       <Outlet />
-    </>
+    </div>
   )
 }
 
