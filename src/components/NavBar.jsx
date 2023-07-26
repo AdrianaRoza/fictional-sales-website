d3c6e86cb28bca4dd1bc8f676e2876f878e9cea5
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-07-26 16:02:51 -0400

    refactor:refactoring navbar responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,40 +36,77 @@ const NavBar = () => {
           className="text-xl mr-3 cursor-pointer" />
       </div>
 
-        <ul 
-          className={`lg:flex px-4 gap-2
-          ${ showNav ? (
-            'flex flex-col absolute right-0 top-14 rounded-lg mr-1'
-          ) : (
-            'hidden'
-          )}`}>
-          <li>
-            <Link to="/" 
-              className="text-green-950 hover:text-teal-500">
-              Home
-            </Link>
-          </li>
-          
-          <li>
-            <Link to="/products" 
-              className="text-green-950  hover:text-teal-500	">
-              Products
-            </Link>
-          </li>
+        {/*Menu for large screens */}
+      <ul className="hidden lg:flex px-4 gap-2">
+        <li>
+          <Link to="/" className="text-green-950 hover:text-teal-500">
+            Home
+          </Link>
+        </li>
 
-          <li>
-            <Link to="/about"
-              className="text-green-950  hover:text-teal-500">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" 
-            className="text-green-950  hover:text-teal-500	">
-              Contact
-            </Link>
-          </li>
-        </ul>
+        <li>
+          <Link to="/products" className="text-green-950 hover:text-teal-500">
+            Products
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/about" className="text-green-950 hover:text-teal-500">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="text-green-950 hover:text-teal-500">
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* Menu for small screens */}
+      <ul
+        className={`${
+          showNav ? 'block' : 'hidden'
+        } lg:hidden absolute right-0 top-14 rounded-lg bg-teal-50 shadow-lg w-48 mt-2`}
+      >
+        <li className="p-2">
+          <Link
+            to="/"
+            className="block text-green-950 hover:text-teal-500 text-center"
+            onClick={() => setShowNav(false)}
+          >
+            Home
+          </Link>
+        </li>
+
+        <li className="p-2">
+          <Link
+            to="/products"
+            className="block text-green-950 hover:text-teal-500 text-center"
+            onClick={() => setShowNav(false)}
+          >
+            Products
+          </Link>
+        </li>
+
+        <li className="p-2">
+          <Link
+            to="/about"
+            className="block text-green-950 hover:text-teal-500 text-center"
+            onClick={() => setShowNav(false)}
+          >
+            About
+          </Link>
+        </li>
+        <li className="p-2">
+          <Link
+            to="/contact"
+            className="block text-green-950 hover:text-teal-500 text-center"
+            onClick={() => setShowNav(false)}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 }
